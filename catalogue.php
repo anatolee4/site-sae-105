fac71cf46f9a6b579c427f68e40cfd1ece4e131c
anatolee4 <anatole.martin@sfr.fr>
2026-02-02 13:02:20 +0100

    produit img

--- catalogue.php
+++ catalogue.php
@@ -16,17 +16,17 @@
             <?php
             $produits = [
                 ["Chevalier Lumière", 89.99, "img/produit1.png", "img/produit1_hover.png"],
-                ["Garde Royal", 119.99, "img/produit2.png", "img/produit2_hover.png"],
-                ["Sentinelle Azur", 95.99, "img/produit3.png", "img/produit3_hover.png"],
-                ["Titan Écarlate", 149.99, "img/produit4.png", "img/produit4_hover.png"],
-                ["Lancier Blanc", 79.99, "img/produit5.png", "img/produit5_hover.png"],
-                ["Commandant Noir", 159.99, "img/produit6.png", "img/produit6_hover.png"],
-                ["Archer Vert", 99.99, "img/produit7.png", "img/produit7_hover.png"],
-                ["Paladin Doré", 169.99, "img/produit8.png", "img/produit8_hover.png"],
-                ["Guerrier Argent", 109.99, "img/produit9.png", "img/produit9_hover.png"],
-                ["Mirage Fantôme", 129.99, "img/produit10.png", "img/produit10_hover.png"],
-                ["Éclat Solaire", 134.99, "img/produit11.png", "img/produit11_hover.png"],
-                ["Ombre Nocturne", 144.99, "img/produit12.png", "img/produit12_hover.png"]
+                ["Garde Royal", 119.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Sentinelle Azur", 95.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Titan Écarlate", 149.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Lancier Blanc", 79.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Commandant Noir", 159.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Archer Vert", 99.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Paladin Doré", 169.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Guerrier Argent", 109.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Mirage Fantôme", 129.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Éclat Solaire", 134.99, "img/produit1.png", "img/produit1_hover.png"],
+                ["Ombre Nocturne", 144.99, "img/produit1.png", "img/produit1_hover.png"]
             ];
 
             foreach ($produits as $p) {
